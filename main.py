5263e959ef9b7b8fc80e92c85619918061da4290
Ahmed EL-KADHI <ael-kadh@e2r3p16.42.fr>
2015-12-09 13:56:37 +0100

    daemon added

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import threading
 import time
 import argparse
 
+from daemon import *
 from shell import *
 from programList import ProgramList
 
@@ -55,11 +56,19 @@ def check_fileExistance(path):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--colors", help="add colors to shell", nargs=1)
+    parser.add_argument("-d", "--daemon", help="run program as daemon", action="store_true")
     parser.add_argument("-f", "--file", help="add configuration file", nargs=1)
+    parser.add_argument("--stop", help="stop the program", action="store_true")
+    # parser.add_argument("--start", help="start", action="store_true")
+    # parser.add_argument("--restart", help="restart", action="store_true")
     args = parser.parse_args()
     fd = None
+    if args.stop:
+        taskMasterStop()
+    if conf["args"].daemon:
+        daemonize()
     if (args.file != None):
-        fd = check_fileExistance(args.file[0])    
+        fd = check_fileExistance(args.file[0])
     if (fd is not None):
         global progs
         progs = ProgramList(fd)
